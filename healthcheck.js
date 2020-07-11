9d4fd97c25828cd8dc2bb75904bc9682bd7e95e3
August Johann <aujo@platynum.ch>
2020-07-11 13:45:29 +0200

    404 is an error

--- healthcheck.js
+++ healthcheck.js
@@ -15,7 +15,7 @@ var options = {
 
 var request = proto.request(options, (res) => {
     console.log(`STATUS: ${res.statusCode}`);
-    if ((res.statusCode >= 200) && (res.statusCode < 500)) {
+    if ((res.statusCode >= 200) && (res.statusCode < 400)) {
         process.exit(0);
     }
     else {
