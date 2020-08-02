4852272d3c1c26013c7ceb1a800b4651fc0ec19b
Noé Deroux <noe.deroux@platynum.ch>
2020-08-02 21:21:56 +0200

    Fixed health check

--- healthcheck.js
+++ healthcheck.js
@@ -6,7 +6,7 @@ var hostname = process.env['HOSTNAME'];
 var options = {
     host: 'localhost',
     port: 443,
-    path: '/download/Sub/ocsp.crt.pem',
+    path: '/download/Sub/ca.crt.pem',
     timeout: 4000,
     checkServerIdentity: function(host, cert) {
         return tls.checkServerIdentity(hostname, cert);
